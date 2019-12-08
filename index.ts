bfdbd99cfca3c8af4d4f2b63246d5d392a9d7ca1
Thomas Roberts <celeissl@gmail.com>
2019-12-08 23:42:42 +0000

    5 and 6 done

--- index.ts
+++ index.ts
@@ -2,8 +2,12 @@ import {taskOne} from './challenges/1';
 import {taskTwo} from './challenges/2';
 import {taskThree} from './challenges/3';
 import {taskFour} from './challenges/4';
+import {taskFive} from './challenges/5';
+import {taskSix} from './challenges/6';
 
 //taskOne();
 //taskTwo();
 //taskThree();
-taskFour();
+//taskFour();
+//taskFive();
+taskSix();
