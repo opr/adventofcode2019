18526602d8fb1e6b0169872715b7f0d9a4ba4375
Thomas Roberts <celeissl@gmail.com>
2019-12-08 11:44:46 +0000

    level 4 done

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import {taskOne} from './challenges/1';
 import {taskTwo} from './challenges/2';
 import {taskThree} from './challenges/3';
+import {taskFour} from './challenges/4';
 
 //taskOne();
 //taskTwo();
-taskThree();
+//taskThree();
+taskFour();
